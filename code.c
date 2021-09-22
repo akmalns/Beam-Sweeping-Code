78d559829205eb581ef77cc60bb47f785c118344
Akmal Narendra Sakti <akmalns@gmail.com>
2021-09-22 09:13:25 +0700

    Hmin Bug Fixed

--- code.c
+++ code.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 #include <math.h>
 
-#define W_BO 50
-#define sigma 10
+#define W_BO_per_sigma 240
 
-int findH_min(int C, int T, int T1, int T2, int i, int j){
-    int x = i%T;
+int minimum(int a, int b){
+    int returnValue;
+    if(a<=b){
+        returnValue = a;
+    }else{
+        returnValue = b;
+    }
+
+    return returnValue;
+}
+
+int maximum(int a, int b){
+    int returnValue;
+    if(a>=b){
+        returnValue = a;
+    }else{
+        returnValue = b;
+    }
+
+    return returnValue;
+}
+
+int findH_min(int i,int C,int j, int T, int P1, int P2){
+    int x;
     int hmin;
+
+    if(P2==0){
+        P2=P1;
+    }
 
     /*
     Case 1 : h will backoff in the 2nd up to Nth frame in a multiframe
@@ -20,19 +45,27 @@ int findH_min(int C, int T, int T1, int T2, int i, int j){
     Case 3 : h will backoff in the very first frame in the very first multiframe
     Note : the minimum value of h will be right before h+Wbo equal to i
     */
-    if((i%T)>T1 && (i%T)<(T-T2)){ //Case 1
-        hmin = (int) ceil((i-x-T1) - (W_BO)/sigma);
-    }else if((i%T)<T1 && (i/T)>1){ //Case 2
-        hmin = (int) ceil((i-x-T2) - (W_BO)/sigma);
-    }else if((i%T)<T1 && (i/T)<1){ //Case 3
-        hmin = (int) ceil((i-x+1) - (W_BO)/sigma);
+    if((i%T)>(j+P1) && (abs(i%T%(j+P1)-1))<j && i%T%(j+P1)!=0){ //Case 1
+        printf("This is case 1\n");
+        x=i%T%(j+P1);
+        hmin = (int) ceil((i-x-P1) - W_BO_per_sigma);
+    }else if((i%T%(j+P1))<(j+P1) && ((float)i/T)>1 && (abs(i%T%(j+P1)-1))<j && i%T%(j+P1)!=0){ //Case 2
+        printf("This is case 2\n");
+        x=i%T%(j+P2);
+        hmin = (int) ceil((i-x-P2) - W_BO_per_sigma);
+    }else if((i%T%(j+P1))<(j+P1) && ((float)i/T)<1 && (abs(i%T%(j+P1)-1))<j && i%T%(j+P1)!=0){ //Case 3
+        printf("This is case 3\n");
+        hmin = (int) ceil((i-x+1) - W_BO_per_sigma);
+    }else{
+        printf("Out of Case");
+        hmin=-1;
     }
     
     return hmin;
 }
 
-int findH_max(int C, int T, int T1, int T2, int i, int j){
-    int x = i%T;
+int findH_max(int C, int T, int T1, int T2, int i){
+    int x;
     int hmax;
 
     /*
@@ -48,22 +81,32 @@ int findH_max(int C, int T, int T1, int T2, int i, int j){
     Note : the minimum value of h will be right before h+Wbo equal to i
     */
     if((i%T)>T1 && (i%T)<(T-T2)){ //Case 1
-        hmax = (int) floor((i-x+1)-(W_BO/sigma));
+        x=i%T%T1;
+        hmax = (int) floor((i-x+1)-(W_BO_per_sigma));
     }else if((i%T)<T1 && (i/T)>1){ //Case 2
-        hmax = (int) floor((i-x+1)-(W_BO/sigma));
+        x=i%T%T2;
+        hmax = (int) floor((i-x+1)-(W_BO_per_sigma));
     }else if((i%T)<T1 && (i/T)<1){ //Case 3
-        hmax = (int) floor((i-x+1)-(W_BO/sigma));
+        x=i%T%T1;
+        hmax = (int) floor((i-x+1)-(W_BO_per_sigma));
+    }else{
+        hmax=-1;
     }
     
     return hmax;
 }
 
-float calculate_alpha(int i, int ){
-    
-}
-
 
 int main(){
+    //Testing hmin function
 
+    int i=0;
+    while(i!=-999){
+        printf("Insert i : ");
+        scanf("%d",&i);
+
+        printf("%d\n",findH_min(i,1,6,60,54,0));
+    }
+    
 }
 
