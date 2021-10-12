676b3bd1ed8c1c5b7d882b38a05b194a347fcd6a
Akmal Narendra Sakti <akmalns@gmail.com>
2021-10-12 17:33:06 +0700

    redefining reattempt as maxattempt, matching ann's definition

--- main_func.c
+++ main_func.c
@@ -5,30 +5,33 @@
 #include <stdlib.h>
 
 
-void main_func(int length, int reattempt, int m, int C, int j, int T, int T1, int T2, int R){
-    
+void main_func(int length, int maxAttempt, int m, int C, int j, int T, int T1, int T2, int R){
+    float totalContending;
+    int raDuration = j;
+
+
     //Allocating memory for UE status information
     float** contendingUE_array = (float**) calloc(length+1,sizeof(float*));
     for(int i=0;i<length+1;i++){
-        contendingUE_array[i]=(float*) calloc(reattempt+2,sizeof(float));
+        contendingUE_array[i]=(float*) calloc(maxAttempt+1,sizeof(float));
     }
 
     float** successUE_array = (float**) calloc(length+1,sizeof(float*));
     for(int i=0;i<length+1;i++){
-        successUE_array[i]=(float*) calloc(reattempt+2,sizeof(float));
+        successUE_array[i]=(float*) calloc(maxAttempt+1,sizeof(float));
     }
 
     float** collidedUE_array = (float**) calloc(length+1,sizeof(float*));
     for(int i=0;i<length+1;i++){
-        collidedUE_array[i]=(float*) calloc(reattempt+2,sizeof(float));
+        collidedUE_array[i]=(float*) calloc(maxAttempt+1,sizeof(float));
     }
 
     //Main Testing Program
     //Initialize contending UE with m on the first attempt (one-shot arrival)
     for(int i=0;i<length+1;i++){
-            for(int j=0;j<reattempt+2;j++){
-                if(i==1 && j==1){
-                    contendingUE_array[i][j]=m; //initiate this with m
+            for(int j=0;j<maxAttempt+1;j++){
+                if(i>=1 && i<=raDuration && j==1){
+                    contendingUE_array[i][j]=(float)1000/raDuration;
                     successUE_array[i][j]=0;
                     collidedUE_array[i][j]=0;
                 }else{
@@ -37,22 +40,24 @@ void main_func(int length, int reattempt, int m, int C, int j, int T, int T1, in
                     collidedUE_array[i][j]=0;
                 }
             }
-    }  
+    }
 
 
     //Calculating Mi, Mis, Mif for all i value
     for(int i=0;i<length+1;i++){
-        if(i>1){
-            calculate_contending_UE(i,contendingUE_array[i],3,collidedUE_array,C,j,T,T1,T2); //Calculate contending UE if i>1 (the first slot is the initial transmission of one-shot arrival process)
+       if(i>1){
+            calculate_contending_UE(i,contendingUE_array[i],maxAttempt,collidedUE_array,C,j,T,T1,T2); //Calculate contending UE if i>1 (the first slot is the initial transmission of one-shot arrival process)
         }
-        calculate_success_UE(contendingUE_array[i],successUE_array[i],reattempt,total_contending_UE(contendingUE_array[i],reattempt),R); //calculate success UE in a particular slot
-        calculate_collided_UE(contendingUE_array[i],collidedUE_array[i],reattempt,total_contending_UE(contendingUE_array[i],reattempt),R); //calculate collided UE in a particular slot
+        totalContending=total_contending_UE(contendingUE_array[i],maxAttempt);
+        calculate_success_UE(contendingUE_array[i],successUE_array[i],maxAttempt,totalContending,R); //calculate success UE in a particular slot
+        calculate_collided_UE(contendingUE_array[i],collidedUE_array[i],maxAttempt,totalContending,R); //calculate collided UE in a particular slot
+        contendingUE_array[i][0]=totalContending;
     }
 
     printf("======This is contending UE======\n\n");
     
     for(int i=1;i<length+1;i++){
-        for(int j=1;j<reattempt+2;j++){
+        for(int j=1;j<maxAttempt+1;j++){
             printf("%f",contendingUE_array[i][j]);
             printf("\t");
         }
@@ -62,7 +67,7 @@ void main_func(int length, int reattempt, int m, int C, int j, int T, int T1, in
     printf("======This is Success UE======\n\n");
     
     for(int i=1;i<length+1;i++){
-        for(int j=1;j<reattempt+2;j++){
+        for(int j=1;j<maxAttempt+1;j++){
             printf("%f",successUE_array[i][j]);
             printf("\t");
         }
@@ -72,14 +77,14 @@ void main_func(int length, int reattempt, int m, int C, int j, int T, int T1, in
     printf("======This is Collided UE======\n\n");
     
     for(int i=1;i<length+1;i++){
-        for(int j=1;j<reattempt+2;j++){
+        for(int j=1;j<maxAttempt+1;j++){
             printf("%f",collidedUE_array[i][j]);
             printf("\t");
         }
         printf("\n");
     }  
 
-    writeToCSV(contendingUE_array,successUE_array,collidedUE_array,length,reattempt);
+    writeToCSV(contendingUE_array,successUE_array,collidedUE_array,length,maxAttempt);
     //Deallocating Memory
     for(int i=0;i<length+1;i++){
         free(contendingUE_array[i]);
